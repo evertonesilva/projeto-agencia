556b891163d60d8709b75e5e2954cf8b410a8456
Everton Evaristo da Silva <138706406+evertonesilva@users.noreply.github.com>
2024-02-08 23:41:03 -0300

    Terminado projeto principal

---
+++ about.js
@@ -0,0 +1,19 @@
+class MobileNavBar{
+    constructor(faBar, navList, navLink) {
+        this.faBars = document.querySelector(fas);
+        this.navList = document.querySelector(nav-container);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active";
+    }
+
+    addClickEvent(){
+        this.faBars.addEventListener("Click", () => console.log("hey"));
+    }
+}
+
+init() {
+    if (this.faBar){
+        this,addClickEvent();
+
+    }
+}
